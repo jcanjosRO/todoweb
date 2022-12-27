ba52b10d0c293a4eba894773112c53acdfee24c8
jcanjosRO <jean.anjos@gmail.com>
2022-12-27 17:54:57 -0400

    Implementado Ação "Adicionar novas tarefas"

--- web.py
+++ web.py
@@ -4,6 +4,11 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["nova_tarefa"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
 st.title("Meu app de Tarefas")
 st.subheader("Este é meu app de tarefas a fazer.")
 st.write("Este App é para aumentar sua produtividade.")
@@ -13,4 +18,7 @@ for todo in todos:
     st.checkbox(todo)
 
 
-st.text_input(label="", placeholder="Digite uma tarefa...")
+st.text_input(label="", placeholder="Digite uma tarefa...",
+              on_change=add_todo, key='nova_tarefa')
+
+print("Ola!!")
