f39790a8d6e75d4005f66c7fb47f1e29cc061a4c
jcanjosRO <jean.anjos@gmail.com>
2023-03-13 22:21:56 -0400

    web todo inicial

--- web.py
+++ web.py
@@ -1,18 +1,18 @@
 import streamlit as st
 import functions
 
-
 todos = functions.get_todos()
+
 
 def add_todo():
     todo = st.session_state["nova_tarefa"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
+
 st.title("Meu app de Tarefas")
 st.subheader("Este é meu app de tarefas a fazer.")
-st.write("Este App é para aumentar sua produtividade.")
-
+st.write("Este App irá aumentar sua produtividade.")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -21,8 +21,5 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
-
-
 st.text_input(label="", placeholder="Digite uma tarefa...",
               on_change=add_todo, key='nova_tarefa')
-
