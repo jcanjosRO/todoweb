613d5794d8296421f7a99c5c5037f7870d5e7ec0
jcanjosRO <jean.anjos@gmail.com>
2023-03-13 20:08:19 -0400

    Alterações em 2023 	modified:   web.py  Alterações em 2023 	modified:   todos.txt 	modified:   web.py  Alterações em 2023

--- web.py
+++ web.py
@@ -14,11 +14,15 @@ st.subheader("Este é meu app de tarefas a fazer.")
 st.write("Este App é para aumentar sua produtividade.")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 
 st.text_input(label="", placeholder="Digite uma tarefa...",
               on_change=add_todo, key='nova_tarefa')
 
-print("Ola!!")
